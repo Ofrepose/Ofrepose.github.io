5d925be239de8710fe9b6ac389425bf87a119494
Ofrepose <haroldfranklinpayne@gmail.com>
2024-07-16 17:55:16 -0400

    for gh pages

--- src/components/brand/Logo.jsx
+++ src/components/brand/Logo.jsx
@@ -1,16 +1,11 @@
-import bugHub1 from 'assets/img/bighub1.png';
+
 
 export const Logo = ({ textExtra, imageExtra }) => {
 
     return (
         <div className='flex items-center'>
             <span className={`text-primaryYellow font-poppins md:text-4xl text-2xl ${textExtra}`}>Bug</span>
-            <img
-                src={bugHub1}
-                alt="Logo"
-                className={`md:px-2 px-1 md:h-14 h-12 ${imageExtra}`}
-                style={{ width: 'auto' }}
-            />
+            
             <span className={`text-primaryBlue font-poppins md:text-4xl text-2xl ${textExtra}`}>Hub</span>
         </div>
     )
